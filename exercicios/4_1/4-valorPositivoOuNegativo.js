d5cf8f789eee2cff9d307be17580447beafbd832
Tiago Emanuel <trsilva284@gmail.com>
2020-03-27 07:57:51 -0300

    Exercicio 4 - verifica positivo, negativo ou zero

---
+++ exercicios/4_1/4-valorPositivoOuNegativo.js
@@ -0,0 +1,15 @@
+let numero = -19;
+
+//Verifica o valor da variável
+if(numero > 0)
+{
+    console.log("Positivo.");
+}
+else if (numero < 0)
+{
+    console.log("Negativo.");  
+}
+else
+{
+    console.log("Zero.");
+}
