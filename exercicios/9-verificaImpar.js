946437b73bfe5aec975dc83c790267e5cfa273a9
Tiago Emanuel <trsilva284@gmail.com>
2020-03-27 09:43:27 -0300

    Exercicio 9 - verifica se e impar

---
+++ exercicios/9-verificaImpar.js
@@ -0,0 +1,15 @@
+let num1= 30;
+let num2=41;
+let num3=10;
+let resultado = null;
+
+if(num1 % 2 ==1 || num2 % 2 ==1 || num3 % 2 ==1)
+{
+    resultado = true;
+}
+else
+{
+    resultado = false;
+}
+
+console.log(resultado);
