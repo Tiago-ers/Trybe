b02f6d4b79ec67f4349856d329a322f318740784
Tiago Emanuel <trsilva284@gmail.com>
2020-03-27 08:23:47 -0300

    Exercicio 5 - triangulo

---
+++ exercicios/4_1/5-triAngulos.js
@@ -0,0 +1,15 @@
+let vert1 = 42;
+let vert2 = 93;
+let vert3 = 45;
+let eUmTriangulo = vert1 + vert2 + vert3;
+
+if(eUmTriangulo == 180)
+{
+    eUmTriangulo = true;
+    console.log("É um triângulo")
+}
+else
+{
+    eUmTriangulo = false;
+    console.log("Não é um triângulo")
+}
