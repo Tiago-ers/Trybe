b6737eb87c925c3cf612217ad1951ea155f2c714
Tiago Emanuel <trsilva284@gmail.com>
2020-03-27 07:44:03 -0300

    Exercicio 3 - comparar tres numeros

---
+++ exercicios/3-maiorDeTres.js
@@ -0,0 +1,17 @@
+let num1 = 59;
+let num2 = 58;
+let num3 = 4;
+
+//comparando variáveis
+if(num1 > num2 && num1 > num3)
+{
+    console.log("O maior numero é : " + num1);
+}
+else if( num2 > num1 && num2 > num3)
+{
+    console.log("O maior numero é : " + num2);
+}
+else
+{
+    console.log("O maior numero é : " + num3);
+}
