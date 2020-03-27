777df207803ba8f2eb0f091e7d4b35fe3520b3c6
Tiago Emanuel <trsilva284@gmail.com>
2020-03-27 09:40:22 -0300

    Exercicio 8 - verifica se e par

---
+++ exercicios/4_1/8-verificaPar.js
@@ -0,0 +1,15 @@
+let num1= 35;
+let num2=47;
+let num3=17;
+let resultado = null;
+
+if(num1 % 2 ==0 || num2 % 2==0 || num3 % 2 ==0)
+{
+    resultado = true;
+}
+else
+{
+    resultado = false;
+}
+
+console.log(resultado);
