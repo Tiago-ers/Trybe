8b40e80ffeb6b03f88fe2280fc04f8366e7b336a
Tiago Emanuel <trsilva284@gmail.com>
2020-03-30 18:08:21 -0300

    exercicio/4.3

---
+++ exercicios/4_2/1-Exercicio.js
@@ -0,0 +1,43 @@
+
+//Exercicio 1
+// let n =5;
+// let linha = '';
+// let caracter = "*";
+
+// for(var i =0; i<n; i++)
+// {          
+//     linha += caracter;
+// } 
+
+//   for (var j =0; j<n; i++)
+//   {  
+//     console.log(linha);
+//   }
+
+
+//Exercicio 2
+/* let caracter = "*";
+
+for(var i =0; i<4; i++)
+{  
+    if( i ==0)
+    {
+     console.log(caracter);
+    }      
+    console.log(caracter += "*");
+} */
+
+//exercicio 3
+let n =5;
+let linha = '';
+let caracter = "*";
+
+for(var i =0; i<n; i++)
+{          
+  linha += caracter;
+} 
+
+for(var j =n; j>0; j--)
+{          
+  linha += " ";
+} 
